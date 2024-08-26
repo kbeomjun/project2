91e3980ddc134181e05f89f2f4780e558f0e0452
kbeomjun <98301971+kbeomjun@users.noreply.github.com>
2024-08-26 17:39:16 +0900

    db - DML_INSERT 추가(토론방)

--- db/mbti_DML_INSERT.sql
+++ db/mbti_DML_INSERT.sql
@@ -26,5 +26,8 @@ insert into personality_type(pt_code, pt_content) values
 ('ESFP', 'ESFP입니다.');
 
 # 토론방 추가(주제 3개 정도 추가)
-
+insert into discussion_room(dr_topic) values
+("오늘 밤에 파티하려고 하는데 너도 와! - 친구가 갑자기 파티에 초대 했을 때 나의 반응은?"),
+("사람이 죽으면 어떻게 될까? 라는 질문을 받았을 때 나의 반응은?"),
+("친구가 택배를 뜯다가 칼에 손이 베였다고 했을 때 나의 반응은?");
 # 질문 추가(qu_type은 IE, NS, TF, JP 4개이고 각 유형별로 5개씩 총 20개 추가)
