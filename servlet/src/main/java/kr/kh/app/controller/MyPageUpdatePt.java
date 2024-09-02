281f84b837edec34a1e21c5937bd023d015d9488
kbeomjun <98301971+kbeomjun@users.noreply.github.com>
2024-09-02 17:39:09 +0900

    성격유형 수정 서블릿 수정

--- servlet/src/main/java/kr/kh/app/controller/MyPageUpdatePt.java
+++ servlet/src/main/java/kr/kh/app/controller/MyPageUpdatePt.java
@@ -18,9 +18,6 @@ public class MyPageUpdatePt extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private MemberService memberService = new MemberServiceImp();
 	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String pt_code = request.getParameter("pt_code");
 		String pt_content = request.getParameter("pt_content");
