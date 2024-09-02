89bbc3fd6673228c9ac9a828c70af83afd08a205
kbeomjun <98301971+kbeomjun@users.noreply.github.com>
2024-09-02 17:46:36 +0900

    마이페이지 서블릿 성격유형 리스트 불러오는 코드 추가

--- servlet/src/main/java/kr/kh/app/controller/MyPage.java
+++ servlet/src/main/java/kr/kh/app/controller/MyPage.java
@@ -1,15 +1,16 @@
 package kr.kh.app.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import kr.kh.app.model.vo.MemberVO;
+import kr.kh.app.model.vo.Personality_typeVO;
 import kr.kh.app.service.MemberService;
 import kr.kh.app.service.MemberServiceImp;
 
@@ -19,6 +20,12 @@ public class MyPage extends HttpServlet {
 	private MemberService memberService = new MemberServiceImp();
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		MemberVO user = (MemberVO)request.getSession().getAttribute("user");
+		if(user != null && user.getMe_authority().equals("ADMIN")) {
+			List<Personality_typeVO> ptList = memberService.getPersonality_typeList();
+			request.setAttribute("ptList", ptList);
+		}
+		
 		request.getRequestDispatcher("/WEB-INF/views/member/mypage.jsp").forward(request, response);
 	}
 
